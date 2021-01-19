2244922fe223cd8a25767a01da0c8501a5014255
Hasib787 <mr.hasib45@gmail.com>
2021-01-19 16:15:07 +0600

    adding a new file

---
+++ constLet.js
@@ -0,0 +1,14 @@
+let myName = 'Hasibul Hasan';
+
+if (myName.length > 4) {
+    myName = 'Hasib';
+}
+console.log(myName);
+
+let age=25;
+age=26;
+age=27;
+console.log(age);
+
+const country='Bangladesh';
+console.log(country);
