00aaf723fe7adc71b451282664eae79d32c2383f
Hasib787 <mr.hasib45@gmail.com>
2021-01-19 15:41:04 +0600

    Adding a file called inchToFeet

---
+++ inchToFeet.js
@@ -0,0 +1,8 @@
+function inchToFeet(inch) {
+    var feet = inch / 12;
+    return feet;
+}
+var grandpaFeet= inchToFeet(540);
+console.log(grandpaFeet);
+var grandmaFeet=inchToFeet(156);
+console.log(grandmaFeet);
