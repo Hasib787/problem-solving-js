74447c17bf9d6bb3eb0fecddc38580c30758fb21
Hasib787 <mr.hasib45@gmail.com>
2021-01-20 18:54:39 +0600

    adding a new file with soluation of prime number

---
+++ checkPrime.js
@@ -0,0 +1,11 @@
+
+function isPrime(n){
+    for (i = 2; i < n; i++) {
+        if (n % i == 0) {
+            return 'Not a prime Number';
+        }
+    }
+    return 'Its a prime number';
+}
+let result=isPrime(23);
+console.log(result);
